8577f08555739d47fca6b52bc186b0ce2a7e6a6b
Miriama Svítková <miri52@users.noreply.github.com>
2021-04-16 16:58:58 +0100

    Initial commit
    - set up the HTML and CSS
    - make the pacman move based on keys pressed
    - Scrimba - Module 6/3 - 1 - 15

---
+++ index.js
@@ -0,0 +1,108 @@
+const width = 28;
+const grid = document.querySelector(".grid")
+const scoreDisplay = document.querySelector("#score");
+let squares = []
+
+//28 * 28 = 784
+  // 0 - pac-dots
+  // 1 - wall
+  // 2 - ghost-lair
+  // 3 - power-pellet
+  // 4 - empty
+const layout = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 
+]
+
+function createBoard(){
+    for (let i = 0; i < layout.length; i++){
+        const square = document.createElement("div");
+        grid.append(square);
+        squares.push(square)
+
+        switch (layout[i]){
+            case 0:
+            squares[i].classList.add("pac-dot");
+            break;
+            case 1:
+            squares[i].classList.add("walls");
+            break;
+            case 2:
+            squares[i].classList.add("ghost-lair");
+            break;
+            case 3:
+            squares[i].classList.add("power-pellet");
+            break;  
+        }
+    }
+}
+
+createBoard()
+
+
+// starting position
+let pacmanCurrentIndex = 490;
+squares[pacmanCurrentIndex].classList.add("pacman")
+
+function control(e){
+    squares[pacmanCurrentIndex].classList.remove("pacman")
+    switch (e.key) {
+        case "ArrowDown":
+            if (!squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') && 
+            !squares[pacmanCurrentIndex + width].classList.contains('walls') &&
+            pacmanCurrentIndex + width < width * width){
+                pacmanCurrentIndex += width 
+            }
+            break;
+        case "ArrowUp":
+            if (!squares[pacmanCurrentIndex - width].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex - width].classList.contains('walls') &&
+            pacmanCurrentIndex - width >= 0){
+                pacmanCurrentIndex -= width
+            }
+            break;
+        case "ArrowLeft":
+            if (!squares[pacmanCurrentIndex - 1].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex - 1].classList.contains('walls') &&
+            pacmanCurrentIndex % width !== 0){
+                pacmanCurrentIndex -= 1
+            }
+            break;
+        case "ArrowRight":
+            if (!squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex + 1].classList.contains('walls') &&
+            pacmanCurrentIndex % width < width - 1){
+                pacmanCurrentIndex += 1
+            }
+            break;
+    }
+    squares[pacmanCurrentIndex].classList.add("pacman")
+}
+
+document.addEventListener("keyup", control)
